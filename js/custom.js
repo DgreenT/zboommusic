2fd6a1e849e148033ca3465273766a0d1d95a1e3
deb <d.a.tavares@outlook.com>
2022-03-23 18:40:24 +0100

    initialcommit

---
+++ js/custom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.responsive').slick({dots:!0,infinite:!0,slidesToShow:3,slidesToScroll:3,responsive:[{breakpoint:1400,settings:{slidesToShow:3,slidesToScroll:1,infinite:!0,dots:!0}},{breakpoint:1200,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:800,settings:{slidesToShow:1,slidesToScroll:1}}]});$('.single').slick({})});AOS.init()
